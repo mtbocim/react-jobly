e1cdeeeb176caf92d78d791676efe1c778994df9
clchen-arcadia <clchen.arcadia@gmail.com>
2022-11-30 17:01:58 -0500

    Write JobCardList.js

--- src/JobCardList.js
+++ src/JobCardList.js
@@ -1,14 +1,21 @@
+import JobCard from "./JobCard.js";
+
 /**
  * Renders a list of JobCards
- * 
+ *
  * State: none
- * Props: jobs as [{title, description, salary, equity}, ...]
- * 
+ * Props: jobs as [{title, salary, equity, companyHandle}, ...]
+ *
  * Currently used by: JobList, CompanyDetail
  */
 
 function JobCardList({jobs}){
 
+  return (
+    <div className="JobCardList">
+      {jobs.map((j, i) => <JobCard jobData={j} key={i}/>)}
+    </div>
+  )
 }
 
 export default JobCardList;

d89a782ed7e439cbbc0c7cc372b68b0ce2b638e0
clchen-arcadia <clchen.arcadia@gmail.com>
2022-11-30 16:54:50 -0500

    Write JobCard.js

--- src/JobCard.js
+++ src/JobCard.js
@@ -1,14 +1,22 @@
 /**
  * Renders a card containing a job title and its details.
- * 
+ *
  * State: none
- * Props: jobData as {title, description, salary, equity}
- * 
+ * Props: jobData as {title, salary, equity, companyHandle}
+ *
  * JobCardList -> JobCard
  */
 
 function JobCard({jobData}){
 
+  return (
+    <div className="JobCard">
+      <h3>{jobData.title}</h3>
+      <p>{jobData.companyHandle}</p>
+      <p>Salary: {jobData.salary}</p>
+      <p>Equity {jobData.equity}</p>
+    </div>
+  )
 }
 
 export default JobCard;

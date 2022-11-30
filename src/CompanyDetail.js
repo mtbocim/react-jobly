3427d18fdf2602d40ee198123753c5c9a8d3d475
clchen-arcadia <clchen.arcadia@gmail.com>
2022-11-30 17:18:19 -0500

    Render JobCardList from CompanyDetail.js

--- src/CompanyDetail.js
+++ src/CompanyDetail.js
@@ -1,27 +1,28 @@
 import {useParams} from 'react-router-dom';
 import {React, useState, useEffect} from 'react'
 import JoblyApi from './JoblyAPI';
+import JobCardList from "./JobCardList.js";
 
 /**
  * Renders a CompanyDetail component
- * 
+ *
  * State: companyDetailPage
  *          {
  *              isLoaded: (bool)
  *              companyData:
  *                  {
- *                      description, 
- *                      handle, 
- *                      jobs:[], 
- *                      name, 
+ *                      description,
+ *                      handle,
+ *                      jobs:[],
+ *                      name,
  *                      numEmployees
  *                  }
  *          }
- * 
+ *
  * Props: none
- * 
+ *
  * App -> RoutesList -> CompanyDetail
- * 
+ *
  * companyData populated by API request
  */
 
@@ -33,10 +34,10 @@ function CompanyDetail(){
         isLoading: true,
         companyData: []
     });
-    
+
     const {isLoading, companyData} = companyDetailPage;
     console.log("companyData>>>>>>>", companyData);
-    
+
     useEffect(function fetchCompanyDataWhenMounted() {
         async function fetchCompanyData() {
             const companyResult = await JoblyApi.getCompany(params.handle);
@@ -49,14 +50,14 @@ function CompanyDetail(){
         }
         fetchCompanyData();
     }, []);
-    
+
     if(isLoading) return <i>Loading...</i>
-    
+
     return(
         <div className="CompanyDetail">
             <h3>{companyData.name}</h3>
             <p>{companyData.description}</p>
-            <p>CompanyDetail</p>
+            <JobCardList jobs={companyData.jobs} />
         </div>
     )
 }
